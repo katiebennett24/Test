7ca297f905bc9061f65ab8d889dc428af44230ab
kansa <kansa@DESKTOP-PHP867O>
2023-02-25 16:36:57 -0600

    Add project files.

---
+++ Test/HomeDemo.Master.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Test
+{
+     public partial class HomeDemo : System.Web.UI.MasterPage
+     {
+          protected void Page_Load(object sender, EventArgs e)
+          {
+
+          }
+
+          protected void btnAccountCreation_Click(object sender, EventArgs e)
+          {
+
+          }
+
+          protected void btnLogin_Click(object sender, EventArgs e)
+          {
+
+          }
+     }
+}
